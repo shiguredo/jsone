898f015c386ac417af38e69e5a11f2a9f1b5242a
Takeru Ohta <phjgt308@gmail.com>
2024-11-18 12:00:10 +0900

    Add types

--- src/jsone.erl
+++ src/jsone.erl
@@ -1,3 +1,23 @@
 -module(jsone).
 
 -export([]).
+
+-export_type([json_value/0,
+              json_boolean/0,
+              json_number/0,
+              json_string/0,
+              json_array/0,
+              json_object/0]).
+
+-type json_value() :: json_number() |
+                      json_string() |
+                      json_array() |
+                      json_object() |
+                      json_boolean() |
+                      null |
+                      undefined.
+-type json_boolean() :: boolean().
+-type json_number() :: number().
+-type json_string() :: binary() | atom().
+-type json_array() :: [json_value()].
+-type json_object() :: #{json_string() => json_value()}.
