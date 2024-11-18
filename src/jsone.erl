a648bad66bc4095cc8190c5272cc7d1d753ef4fb
Takeru Ohta <phjgt308@gmail.com>
2024-11-18 12:16:42 +0900

    必要になりそうな型を追加

--- src/jsone.erl
+++ src/jsone.erl
@@ -7,7 +7,10 @@
               json_number/0,
               json_string/0,
               json_array/0,
-              json_object/0]).
+              json_object/0,
+
+              encode_option/0,
+              decode_option/0]).
 
 -type json_value() :: json_number() |
                       json_string() |
@@ -21,3 +24,14 @@
 -type json_string() :: binary() | atom().
 -type json_array() :: [json_value()].
 -type json_object() :: #{json_string() => json_value()}.
+
+-type encode_option() :: skip_undefined |
+                         undefined_as_null |
+                         {float_format, [float_format_option()]}.
+
+-type decode_option() :: {keys, attempt_atom}.
+
+-type float_format_option() :: {scientific, Decimals :: 0..249} |
+                               {decimals, Decimals :: 0..253} |
+                               compact |
+                               short.
